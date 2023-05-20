a4e0fa082bd2f16b6041022eb3a8207f01481c88
Iva Havranová <havranova.i@seznam.cz>
2023-05-20 17:25:49 +0200

    done

--- src/Star/index.jsx
+++ src/Star/index.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import './style.css';
 
-const Star = ({ glowing }) => {
-  // const [glowing, setGlowing] = useState(false);
-
+const Star = ({ rating, onSelect, glowing }) => {
   const starClass = glowing ? 'rating__star rating__star--on' : 'rating__star';
-  // const hvezdoSvit = () => {
-  //   starClass !== 'rating__star rating__star--on' ? onGlow() : onDim();
-  //   setGlowing(!glowing);
-  // };
-  return <div /*onClick={hvezdoSvit}*/ className={starClass}></div>;
+
+  const handleClick = () => {
+    onSelect(rating);
+  };
+  return <div onClick={handleClick} className={starClass}></div>;
 };
 
 export default Star;
